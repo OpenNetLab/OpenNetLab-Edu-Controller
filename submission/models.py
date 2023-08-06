368f762100df208e78c62c25308bd689a63be8ae
zdszero <1335908147@qq.com>
2023-08-06 12:37:00 +0000

    fix student can't get sub detail

--- submission/models.py
+++ submission/models.py
@@ -38,7 +38,7 @@ class Submission(models.Model):
 
     def check_user_permission(self, user, check_share=True):
         if (
-            self.user_id == user.id
+            str(self.user_id) == str(user.id)
             or user.is_super_admin()
             or user.can_mgmt_all_problem()
             or self.problem.created_by_id == user.id
